c94650c13e85bd4269a2d5528e16558a13140dba
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 00:43:58 +0200

    first commit

---
+++ ConsoleAppTest2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String fullname;
+            fullname = "Alexander Alexander Crawu";
+            Console.WriteLine("My name is: " + fullname);
+        }
+    }
+}

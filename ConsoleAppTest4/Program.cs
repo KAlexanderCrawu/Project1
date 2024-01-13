d0a2d2921d4110310620e9599abd238edabf58af
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 01:55:20 +0200

    first commit

---
+++ ConsoleAppTest4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("This is the first line!!!");
+        Console.WriteLine("This is the second line!!");
+        Console.WriteLine("This is the third line!!!");
+    }
+}

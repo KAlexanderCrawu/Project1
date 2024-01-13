9f94548a9c3f3efbcf38bb862f477a77a94af211
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 01:52:03 +0200

    first commit

---
+++ ConsoleAppTest3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        int a;
+        int b;
+        int sum;
+        a = 5;
+        b = 3;
+        sum = a + b;
+        Console.WriteLine("The Sum of 5 and 3 is: " +sum);
+    }
+}

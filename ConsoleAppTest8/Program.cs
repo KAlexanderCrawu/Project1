4b86bb9191f7e7aecca2e15d03ee825cb30cdbfc
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 02:07:59 +0200

    first commit

---
+++ ConsoleAppTest8/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml.Schema;
+class Program
+{
+    //define the main method, the entry point of the program.
+    static void Main()
+    {
+        //Print a message for an enter text
+        Console.WriteLine("Hello, World!");
+
+    }
+}

ff4dfa632c9283b74837fe498666b7770e72f776
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 02:03:25 +0200

    first commit

---
+++ ConsoleAppTest6/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+    static void Main()
+    {
+        String first = "Hello";
+        string second="World!!";
+        Console.WriteLine(first+ " " +second);
+    }
+}

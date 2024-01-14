070d5bfbd563b9cd965659cbb21373ae61b58df0
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 02:11:59 +0200

    first commit

---
+++ Consoletest9/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        int num1;
+        num1 = 10;
+        Console.WriteLine("My favorite number is: " +num1);
+    }
+}

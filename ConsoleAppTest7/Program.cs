eb3e68c8209fed12e22a6c6d45fa69cf989bb23e
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 02:05:59 +0200

    first commit

---
+++ ConsoleAppTest7/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a line of text");
+        string userInput = Console.ReadLine();
+        Console.WriteLine("Your result is: " + userInput);
+    }
+}

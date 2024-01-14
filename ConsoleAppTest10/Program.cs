985dbce2dd190c7c62b749d75d0734e9a5d35d6d
KpawardACrawu <kcrawu@gmail.com>
2024-01-14 02:14:46 +0200

    first commit

---
+++ ConsoleAppTest10/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Microsoft.VisualBasic;
+class Program
+{
+    static void Main()
+    {
+        DateTime currentdate = DateTime.Now;
+        Console.WriteLine("The current date and time is: " + currentdate);
+
+    }
+}

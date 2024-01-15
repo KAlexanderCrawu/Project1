74fb06a016ac5dd5695120862326e461126e8f67
Alexander Kpaward Crawu <156548358+KAlexanderCrawu@users.noreply.github.com>
2024-01-15 14:46:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5; int b = 5; int sum = a + b; int difference = a - b; int quotient = a / b; int product = a * b;
+            Console.WriteLine("The sum of the first and second digit is :" + sum);
+            Console.WriteLine("The difference of the first and second digit is: " +difference);
+            Console.WriteLine("The quotient of the first and second digit is: " + quotient);
+            Console.WriteLine("The product of the first and second digit is: " +  product);
+        }  
+
+    }
+}
